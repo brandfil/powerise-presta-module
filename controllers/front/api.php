4373e31c95b7c70287d061e11ea738762aef509c
Sebastian Lewandowski <sebastian.lewandowski@nestcode.pl>
2023-04-20 17:58:33 +0200

    Map products in API controller

--- controllers/front/api.php
+++ controllers/front/api.php
@@ -1,12 +1,30 @@
 <?php
 
+use Powerise\PrestaShop\Mapper\ProductMapper;
+
 class PoweriseApiModuleFrontController extends ModuleFrontController
 {
     const PRODUCTS_BATCH_SIZE = 50;
 
+    const ACTION_PRODUCTS = 'products';
+
     public function postProcess()
     {
         $page = Tools::getValue('page');
+        $action = Tools::getValue('action');
+
+        switch($action) {
+            case self::ACTION_PRODUCTS:
+                return die(json_encode($this->getProducts($page)));
+            default:
+                http_response_code(400);
+                return die(json_encode(['error' => 'Invalid action.']));
+        }
+    }
+
+    private function getProducts($page = 1)
+    {
+        $productMapper = new ProductMapper();
         $products = \Product::getProducts(
             $this->context->language->id,
             ($page - 1) * self::PRODUCTS_BATCH_SIZE,
@@ -16,6 +34,7 @@ class PoweriseApiModuleFrontController extends ModuleFrontController
             false,
             true
         );
-        return die(json_encode($products));
+
+        return $productMapper->mapArray($products);
     }
 }
---
+++ src/Mapper/ProductMapper.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Powerise\PrestaShop\Mapper;
+
+class ProductMapper
+{
+    public function mapArray(array $products)
+    {
+        return array_map([$this, 'map'], $products);
+    }
+
+    public function map($product)
+    {
+        $link = new \Link();
+        $image = \Image::getCover($product['id_product']);
+        return [
+            'name' => $product['name'],
+            'sku' => $product['reference'],
+            'image' => $link->getImageLink($product['link_rewrite'], $image['id_image'], 'home_default'),
+            'description' => $product['description'],
+            'price' => $product['price'],
+        ];
+    }
+}
